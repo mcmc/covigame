0861e488f2dbc544c1fc17e85543c1fb2db52f7b
Matthew Cook <cook@paradise.caltech.edu>
2020-04-12 19:29:07 +0200

    v0.3 - improved text, y-axis, and playability

--- covigame.js
+++ covigame.js
@@ -1,21 +1,24 @@
+// covigame - Coronavirus Game
+var version = "v0.3";
 
-//////////////////////////////////////////////////          Interactive Simulator
-
+// our canvas
 var frame_w = 640;
 var frame_h = 480;
 var cvpaper = Raphael("cvpaper", frame_w, frame_h);
 
-// a parabola, as a Bezier curve, has x varying linearly and y quadratically
-
-var max_factor = 6.0; // max settable transmission factor
-var start_factor = 5.0; // but it starts at this level
-var current_factor = start_factor;
-var i; // for loops
-
 // picture frame
 cvpaper.rect(1, 1, frame_w - 2, frame_h - 2, 10).attr({stroke: "#666"});
 
-// big green parabola coordinate system, used for many things
+// version number
+cvpaper.text(15, frame_h - 10, version)
+            .attr({fill:"#aaa"});
+
+// parameters that affect the initial layout
+var max_factor = 5.0; // max settable transmission factor
+var start_factor = 3.0; // but it starts at this level
+var current_factor = start_factor;
+
+// coordinate system for action area elements
 var curve_left = 100, curve_right = frame_w - 50, curve_top = 40, curve_bottom = frame_h - 90;
 function pxl_x(percentage) { // 0 to 1
     return curve_left + (curve_right - curve_left) * percentage;
@@ -28,7 +31,60 @@ function grht(percentage) { // green height: parabola height (y) at that percent
     return current_factor * untouched * untouched; // there's the parabola right there
 }
 
-// herd immunity line
+// y-axis label
+cvpaper.text(20, (curve_top + curve_bottom) / 2, "Transmission Factor")
+        .attr({fill: "#777", "font-size": 24})
+        .transform("r270");
+// y-axis numbers
+for (var i = 0.0; i <= max_factor; i += 0.5) {
+    // first the naive position
+    var x = 60;
+    var y = pxl_y(i);
+    // and now tilt it up
+    y = (x - curve_right) / (curve_left - curve_right) * (y - curve_bottom) + curve_bottom;
+    var angle = 180 / Math.PI * Math.atan2(pxl_y(0) - y, pxl_x(1) - x);
+    // and add it to the page
+    cvpaper.text(x, y, i).attr({fill:"#777", "font-size":18}).rotate(angle);
+}
+// y-axis tick marks
+for (var i = 0; i <= 10 * max_factor; i += 1) {
+    // first the naive line
+    var  left_end_x = curve_left - 20;
+    var right_end_x = curve_left - 10;
+    var  left_end_y = pxl_y(0.1 * i);
+    var right_end_y = pxl_y(0.1 * i);
+    // and now tilt it up
+    left_end_y = (left_end_x - curve_right) / (curve_left - curve_right) * (left_end_y - curve_bottom) + curve_bottom;
+    right_end_y = (right_end_x - curve_right) / (curve_left - curve_right) * (right_end_y - curve_bottom) + curve_bottom;
+    // and add it to the page
+    cvpaper.path([["M",               left_end_x,               left_end_y],
+                  ["l", right_end_x - left_end_x, right_end_y - left_end_y] ])
+            .attr({stroke:"#777", "stroke-width":(i % 5 == 0 ? 3 : 1)});
+}
+
+// x-axis label
+cvpaper.text((curve_left + curve_right) / 2, curve_bottom + 60,
+             "% of population that has caught coronavirus")
+        .attr({fill: "#777", "font-size": 22});
+// x-axis numbers
+for (var i = 0; i <= 100; i += 10) {
+    cvpaper.text(pxl_x(0.01 * i) + 5, curve_bottom + 33, i + "%")
+            .attr({fill:"#777", "font-size":14});
+}
+// x-axis tick marks
+for (var i = 0; i <= 100; i += 1) {
+    cvpaper.path([["M", pxl_x(0.01 * i), curve_bottom + 10],
+                  ["l", 0, 10]])
+            .attr({stroke:"#777", "stroke-width":(i % 5 == 0 ? 3 : 1)});
+}
+
+// yellow herd immunity line
+for (var i = 0; i < max_factor + 0.0001; i += 0.5) {
+    cvpaper.path([  ["M", pxl_x(1), pxl_y(0)],
+                    ["L", pxl_x(0), pxl_y(i)]  ])
+                .attr({ "stroke-width":(i == Math.round(i) ? 2 : 1),
+                        "stroke": "#eee" });
+}
 var overreach = 1.0;
 cvpaper.path([["M", curve_right + overreach * (curve_left - curve_right),
                    curve_bottom + overreach * (curve_top - curve_bottom) * 1.0 / max_factor],
@@ -46,43 +102,19 @@ cvpaper.text(pxl_x(0.15), pxl_y(hi_lower), "epidemic shrinks")
               });
 
 // big green parabola
-var magenta_dot =
-    parab(curve_left, pxl_y(start_factor), curve_right, curve_bottom, "hsb(.3, .75, .75)");
-// y-axis label
-cvpaper.text(20, (curve_top + curve_bottom) / 2, "Transmission Factor")
-        .attr({fill: "#777", "font-size": 24})
-        .transform("r270");
-// y-axis numbers
-for (i = 0.0; i <= max_factor; i += 0.5) {
-    cvpaper.text(60, pxl_y(i), i)
-            .attr({fill:"#777", "font-size":18});
-}
-// y-axis tick marks
-for (i = 0; i <= 10 * max_factor; i += 1) {
-    cvpaper.path([["M", curve_left - 20, pxl_y(0.1 * i)],
-                  ["l", 10, 0]])
-            .attr({stroke:"#777", "stroke-width":(i % 5 == 0 ? 3 : 1)});
-}
-// x-axis label
-cvpaper.text((curve_left + curve_right) / 2, curve_bottom + 60,
-             "% of population that has caught coronavirus")
-        .attr({fill: "#777", "font-size": 22});
-// x-axis numbers
-for (i = 0; i <= 100; i += 10) {
-    cvpaper.text(pxl_x(0.01 * i) + 5, curve_bottom + 33, i + "%")
-            .attr({fill:"#777", "font-size":14});
-}
-// x-axis tick marks
-for (i = 0; i <= 100; i += 1) {
-    cvpaper.path([["M", pxl_x(0.01 * i), curve_bottom + 10],
-                  ["l", 0, 10]])
-            .attr({stroke:"#777", "stroke-width":(i % 5 == 0 ? 3 : 1)});
-}
+var green_path = [ ["M", pxl_x(0.0), pxl_y(start_factor)],
+                   ["Q", pxl_x(0.5), pxl_y(           0),    // quadratic control point
+                         pxl_x(1.0), pxl_y(           0)] ];
+var green_curve = cvpaper.path(green_path)
+                            .attr({"stroke": "hsb(.3, .75, .75)",
+                                   "stroke-width": 4,
+                                   "stroke-linecap": "round"});
+
 // people
-var num_people = 150;
+var num_people = 130;
 var people = cvpaper.set();
 var offset = 0;
-for (i = 0; i < num_people; i++) {
+for (var i = 0; i < num_people; i++) {
     offset += 0.2 + 0.6 * Math.random(); if (offset > 1) offset -= 1;
     var percentage = i / num_people;
     var dude_path = [["M", pxl_x(percentage) + 2,
@@ -98,7 +130,7 @@ for (i = 0; i < num_people; i++) {
     dude.memory = {path:dude_path, offset:offset};
     people.push(dude);
 }
-people.attr({"stroke": "purple", "stroke-width": 2}); // color is set in init anyway
+people.attr({"stroke": "blue", "stroke-width": 2}); // color is set in init anyway
 // functions for lifting and lowering the people
 Raphael.el.elevate = function (pct) {
     this.memory.path[0][2] = pxl_y(grht(pct)) - 4 - this.memory.offset * 40;
@@ -110,6 +142,7 @@ Raphael.st.elevate = function () {
         this[who].elevate(who / num_people);
     }
 };
+
 // virus
 var mem = { size: 40, pct: 0.30, x_offset: 0, y_offset: -30 };
 var virus = cvpaper.image("cvmodified.png",
@@ -121,31 +154,86 @@ virus.memory = mem;
 var pct_dot = cvpaper.circle(pxl_x(     virus.memory.pct ),
                              pxl_y(grht(virus.memory.pct)), 3)
                         .attr({fill:"hsb(.3, .75, .5)"});
-magenta_dot.toFront(); // it gets obscured by pct_dot
-//var rolling = Raphael.animation({ 0.0: {transform: "r0"}, // transforms are wrt initial state
-//                                     1.0: {transform: "r360"}
-//                                    }, 1800).repeat(Infinity);
-//virus.animate(rolling); // only one animation can occur at a time, a new one will displace this
-// it is now rotated according to its position
+
+// dragging the curve
+var magenta_dot = cvpaper.circle(pxl_x(0), pxl_y(start_factor), 10)
+                            .attr({fill: "#f0f", stroke: "none"});
+magenta_dot.our_dragstart = function() {
+    this.total_drag_x = 0;
+    this.total_drag_y = 0;
+}
+magenta_dot.our_dragmove = function(new_x, new_y) { // args give offset from drag start
+    // drag bureaucracy
+    var delta_x = new_x - (this.total_drag_x || 0);
+    var delta_y = new_y - (this.total_drag_y || 0);
+    this.total_drag_x = new_x;
+    this.total_drag_y = new_y;
+    var new_x = this.attr("cx");
+    var new_y = this.attr("cy") + delta_y; // move the location in y only
+    if (new_y < curve_top)     new_y = curve_top;    // don't go past top
+    if (new_y > curve_bottom)  new_y = curve_bottom; // don't go past bottom
+    // drag dot
+    if (green_path[0][2] == new_y) return; // it didn't move in y, so we have nothing to do
+    this.attr({cx: new_x, cy: new_y }); // if we want drag to move object, we have to move it
+    // set values that dot controls other items via
+    green_path[0][2] = new_y;
+    current_factor = max_factor * (new_y - curve_bottom) / (curve_top - curve_bottom);
+    // only update view if not already queued
+    if (view_is_up_to_date) {
+        view_is_up_to_date = false;
+        setTimeout(update_view, 0);
+    } // otherwise the call to fix it is already queued
+}
+var view_is_up_to_date = true; // update_view() only callable if this is true
+function update_view() {
+    view_is_up_to_date = true;
+    green_curve.attr({path: green_path});
+    people.elevate();
+    virus.attr({ x: pxl_x(     virus.memory.pct ) + virus.memory.x_offset - virus.memory.size / 2,
+                 y: pxl_y(grht(virus.memory.pct)) + virus.memory.y_offset - virus.memory.size / 2 })
+            .transform("r" + 360 * (virus.memory.pct * (curve_right - curve_left) / virus.memory.size));
+    pct_dot.attr({ cx: pxl_x(     virus.memory.pct ),
+                   cy: pxl_y(grht(virus.memory.pct)) });
+}
+magenta_dot.drag(magenta_dot.our_dragmove, magenta_dot.our_dragstart);
 
 //////////////////////////////////// simulation /////////////////////////////////
 // ticking engine
-var master_clock_seconds = 0;   // this is our definition of what time it is for the game
-var simulation_speed = 3.0;     // how many days per second
-var simulation_t0 = 0;
+var master_clock_ms = Date.now(); // this is our definition of what time it is for the game
+var simulation_t0; // this should be deprecated -- just add something to day clock
+var simulation_speed = 0.003;     // how many days per millisecond
 var tick_interval = 0.020;       // 50 fps
-reset_simulation();
-var useTimeout = 1;
-let timerId = (useTimeout ? setTimeout : setInterval)
-                        (
-                            function tick() {
-                                master_clock_seconds += tick_interval;
-                                master_clock_days = (master_clock_seconds - simulation_t0) * simulation_speed;
-                                time_passed();
-                                if (useTimeout) timerId = setTimeout(tick, tick_interval * 1000);
-                            },
-                            tick_interval * 1000
-                        );
+const useTimeout = 0;
+var paused = 0, pause_time;
+let timerId =
+    (useTimeout ? setTimeout : setInterval) (
+        function tick() {
+            if (paused) {
+                if (document.hasFocus) {
+                    paused = 0;
+                    var delay = Date.now() - pause_time;
+                    simulation_t0 += delay;
+                    tick_interval /= 10; // stop hibernating
+                    // we'll resume simulation on next pass
+                }
+            } else if (document.hasFocus()) {
+                var current_time = Date.now();
+                var delta = current_time - master_clock_ms;
+                var excess_ms = delta - 1 / simulation_speed;
+                if (excess_ms > 0)
+                    simulation_t0 += excess_ms;
+                master_clock_ms = current_time;
+                master_clock_days = (master_clock_ms - simulation_t0) * simulation_speed;
+                time_passed();
+            } else { // running but lost focus: pause
+                paused = 1;
+                pause_time = Date.now();
+                tick_interval *= 10; // start hibernating
+            }
+            if (useTimeout) timerId = setTimeout(tick, tick_interval * 1000);
+        },
+        tick_interval * 1000
+    );
 // simulation parameters
 var days_to_recover = 21;
 var population = 329491234;
@@ -160,14 +248,15 @@ var proportion_recovered;
 var eco_damage_total;
 var history;                // history[d] stores stats for day d
 // init
+reset_simulation(); // Start!
 function reset_simulation() {
-    simulation_t0 = master_clock_seconds;
+    simulation_t0 = master_clock_ms;
     master_clock_days = 0;
     last_time = 0;
     next_casualty = 0;
     next_recovery = 0;
-    proportion_sick = 0;
-    proportion_infectable = 1.0;
+    proportion_sick = 0.003;
+    proportion_infectable = 1.0 - proportion_sick;
     proportion_recovered = 0;
     eco_damage_total = 0;
     history = [];
@@ -205,7 +294,7 @@ function time_passed() { // this is the function that updates the whole simulati
     if (new_day && today >= days_to_recover + 1) {
         var then = today - days_to_recover;
         var starting_untouched = ((then - 1) in history) ? history[then - 1].naive : 1.0;
-                // notice the "in" built-in -- tests existence of key, value is irrelevant
+                // notice the "in" built-in -- tests existence of key -- value is irrelevant
                 // use of absent key generates error & returns special value undefined (not string 'undefined')
                 // but of course special value undefined can also be the value at that key
         var ending_untouched = (then in history) ? history[then].naive : 1.0;
@@ -253,7 +342,7 @@ function time_passed() { // this is the function that updates the whole simulati
 // variable display
 var num_chart_vars = 10;
 cvpaper.setStart();
-for (i = 0; i < num_chart_vars; i++)
+for (var i = 0; i < num_chart_vars; i++)
     cvpaper.text(500, 100 + 15 * i, "");
 var chart = cvpaper.setFinish();
 
@@ -269,74 +358,14 @@ function numstr(n) {
 }
 
 // restart button
-var btn_x = 170, btn_y = 30;
-var btn_xr = 30, btn_yr = 10;
-cvpaper.setStart();
-    cvpaper.rect(btn_x - btn_xr,
-                 btn_y - btn_yr, 2 * btn_xr,
-                                 2 * btn_yr, 10)
-            .attr({ fill: "#f90", stroke: "#420", "stroke-width": 2 });
-    cvpaper.text(btn_x, btn_y, "Restart").attr({ fill: "#000" });
-var restart_button = cvpaper.setFinish();
-if (0) { // this slows down the overall responsiveness, makes it slightly jumpier
-    restart_button[0].hoverInBounds(
-            function(){
-                restart_button.animate({transform: "S1.2"}, 200, "<>");
-            },
-            function(){
-                restart_button.animate({transform: "S1.0"}, 200, "<>");
-            });
-}
-restart_button.click(function(evt){
-                        reset_simulation();
-                        restart_button.animate({0.5: {transform: "S1.2"},
-                                                1.0: {transform: "S1.0"}}, 200, "<>");
-                    });
+var restart_button = cvpaper.button({ cx:170, cy:30, w:60, h:20,
+                                    action:reset_simulation,
+                                    text:"Restart",
+                                    fill: "#f90", stroke: "#420", "stroke-width": 2 });
 
-// creating the parabola
-function parab(x_left, y_top, x_right, y_bottom, color) {
-    // parabolic line
-    var path = [["M", x_left, y_top], ["Q", (x_left + x_right) / 2, y_bottom,
-                                            x_right, y_bottom]];
-    var curve = cvpaper.path(path)
-                        .attr({"stroke": color || Raphael.getColor(),
-                               "stroke-width": 4,
-                               "stroke-linecap": "round"});
-    var control_dot = cvpaper.circle(x_left, y_top, 5)
-                                .attr({fill: "#f0f", stroke: "none"});
-    control_dot.update = function (dx, dy) {
-        var new_x = this.attr("cx");
-        var new_y = this.attr("cy") + dy;
-        if (new_y < curve_top)     new_y = curve_top;    // don't go past top
-        if (new_y > curve_bottom)  new_y = curve_bottom; // don't go past bottom
-        if (path[0][2] == new_y)
-            return; // it didn't move in y, so we have nothing to do
-        this.attr({cx: new_x,
-                   cy: new_y });
-        path[0][2] = new_y;
-        current_factor = max_factor * (new_y - curve_bottom) / (curve_top - curve_bottom);
-        curve.attr({path: path});
-        people.elevate();
-        virus.attr({ x: pxl_x(     virus.memory.pct ) + virus.memory.x_offset - virus.memory.size / 2,
-                     y: pxl_y(grht(virus.memory.pct)) + virus.memory.y_offset - virus.memory.size / 2 })
-                .transform("r" + 360 * (virus.memory.pct * (curve_right - curve_left) / virus.memory.size));
-        pct_dot.attr({ cx: pxl_x(     virus.memory.pct ),
-                       cy: pxl_y(grht(virus.memory.pct)) });
-        pct_dot.toFront();
-        control_dot.toFront();
-    };
-    control_dot.drag(onmove, onstart);
-    return control_dot;
-}
-// dragging the parabola's end  (should be unified with previous function-in-function)
-function onmove(new_x, new_y) {
-    this.update(new_x - (this.last_x || 0),
-                new_y - (this.last_y || 0));
-                        // the default is 0 if they were never set before
-    this.last_x = new_x;
-    this.last_y = new_y;
-}
-function onstart() {
-    this.last_x = 0;
-    this.last_y = 0;
-}
+// time slider
+// var time_slider = cvpaper.slider({ cx:370, cy:30, w:60, h:20,
+//                                     action:reset_simulation,
+//                                     text:"Restart",
+//                                     fill: "#f90", stroke: "#420", "stroke-width": 2 });
+
--- raphael-additions.js
+++ raphael-additions.js
@@ -25,35 +25,73 @@ Raphael.el.hoverInBounds = function(inFunc, outFunc) {
 
 ////////////////////////////////// button //////////////////////////////////
 
-function button(properties) {   // {cx:, cy:, w:, h:,
-                                //  action:
-                                //  text:, font:, textcolor:
-                                //  fill:, stroke:, strokewidth:, rounding:,
-                                //  hover:, unhover:, clickdown:, clickout:
-                                // }
-    var btn_cx = properties?.cx ?? frame_w / 2;
-    var btn_cy = properties?.cy ?? frame_h / 2;
-    var btn_w  = properties?.w  ?? frame_w / 5;
-    var btn_h  = properties?.h  ?? frame_h / 5;
-    var btn_x = cx - w / 2;
-    var btn_y = cy - h / 2;
-    var btn_xr = w / 2;
-    var btn_yr = h / 2;
-    cvpaper.setStart();
-        cvpaper.rect(btn_x, btn_y, btn_w, btn_h, properties?.rounding ?? 10)
-                .attr({ fill:   (properties?.fill ?? "#04c"),
-                        stroke: (properties?.fill ?? "#000"),
-                "stroke-width": (properties?.strokewidth ?? 2) });
-        cvpaper.text(btn_cx, btn_cy, (properties?.text ?? "OK"))
-                .attr({ fill: (properties?.textcolor ?? "#000"),
-                        font: (properties?.font ?? "sans-serif") });
-                                // safe: "courier", "serif", "sans-serif", "fantasy"
-    var new_button = cvpaper.setFinish();
-    new_button.click(function(evt){
-                        (properties?.action ?? function(){})();
-                        new_button.animate({0.5: {transform: "S1.2"},
-                                            1.0: {transform: "S1.0"}}, 200, "<>");
-                    });
+Raphael.fn.button = function(properties) {  // {cx:, cy:, w:, h:, 
+                                            //  action:
+                                            //  text:, font:, textcolor:
+                                            //  fill:, stroke:, strokewidth:, rounding:,
+                                            //  hover:, unhover:, clickdown:, clickout:
+                                            // }
+    var btn_cx = properties.cx;
+    var btn_cy = properties.cy;
+    var btn_w  = properties.w;
+    var btn_h  = properties.h;
+    var btn_x = btn_cx - btn_w / 2;
+    var btn_y = btn_cy - btn_h / 2;
+    var btn_xr = btn_w / 2;
+    var btn_yr = btn_h / 2;
+    this.setStart();
+    this.rect(btn_x, btn_y, btn_w, btn_h, properties?.rounding ?? 10)
+            .attr({ fill:           (properties?.fill ?? "#04c"),
+                    stroke:         (properties?.stroke ?? "#000"),
+                    "stroke-width": (properties?.strokewidth ?? 2) });
+        this.text(btn_cx, btn_cy, (properties?.text ?? "OK"))
+            .attr({ fill: (properties?.textcolor ?? "#000"),
+                    font: (properties?.font ?? "fantasy") });
+                        // safe: "courier", "serif", "sans-serif", "fantasy"
+    var new_button = this.setFinish();
+    new_button.click(function(evt){ (properties?.action ?? function(){})(); });
     return new_button;
 }
+
+
+
+////////////////////////////////// slider //////////////////////////////////
+
+function slider(properties) { // {cx:, cy:, w:, h:,
+                                        //  action:
+                                        //  text:, font:, textcolor:
+                                        //  fill:, stroke:, strokewidth:, rounding:,
+                                        //  hover:, unhover:, clickdown:, clickout:
+                                        // }
+    paper.rect(cx - w / 2, cy - h / 2, w, 0.7 * h, 5).attr({fill: "#7bf", stroke: "#07f"});
+    var slider_knob = paper.ellipse(cx, cy, 0.4 * h, h)
+                           .attr({fill: "#07f", stroke: "#037"})
+                           .drag(onmove, onstart);
+    function onstart() {
+        this.total_dragged_x = 0; // "last" is current mouse position relative to drag start
+        this.total_dragged_y = 0;
+    }
+    function onmove(new_total_dragged_x, new_total_dragged_y) {
+        // drag bureaucracy
+        delta_x = new_total_dragged_x - (this.total_dragged_x || 0);
+        delta_y = new_total_dragged_y - (this.total_dragged_y || 0);
+        this.total_dragged_x = new_total_dragged_x;
+        this.total_dragged_y = new_total_dragged_y;
+        var new_x = this.attr("cx") + delta_x;
+        var new_y = this.attr("cy"); // move the location in x only
+        if (new_x < cx - w / 2)  new_x = cx - w / 2; // don't fall off the end
+        if (new_x > cx + w / 2)  new_x = cx + w / 2;
+        this.attr({cx: new_x, cy: new_y }); // drag doesn't move it, so we move it ourself
+        (properties?.action ?? function(){})();
+    }
+}
+
+
+
+////////////////////////////////// graph //////////////////////////////////
+
+
+
+////////////////////////////////// transport //////////////////////////////////
+
 
